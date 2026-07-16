65b29c3f2e67f1e05b3fd8253342c08bf37381b1
Connor Shore <cjshore12@gmail.com>
2026-07-15 22:25:24 -0600

    cleanup

--- ZombieFPSGame/GameData/Assets/Scripts/SpawnManager.lua
+++ ZombieFPSGame/GameData/Assets/Scripts/SpawnManager.lua
@@ -22,17 +22,14 @@ function SpawnManager:OnCreate(entity)
             local spawnerEntity = Scene.GetEntityByUUID(childUUID)
             if spawnerEntity and spawnerEntity:IsValid() then
                 table.insert(self.Spawners, spawnerEntity)
-                Log.Info("SpawnManager added spawner: " .. spawnerEntity:GetName())
             end
         end
     end
 
     -- Listen for zombie deaths to free up our concurrent spawn slots!
     EventManager.Subscribe("OnEnemyKilled", function(enemyUUID)
-        Log.Info("SpawnManager received OnEnemyKilled event for UUID: " .. tostring(enemyUUID))
         if self.ActiveZombies > 0 then
             self.ActiveZombies = self.ActiveZombies - 1
-            Log.Info("SpawnManager: Active Zombies = " .. tostring(self.ActiveZombies))
         end
     end)
 end
@@ -59,13 +56,10 @@ function SpawnManager:OnUpdate(entity, delta)
     end
 
     self.TimeSinceLastSpawn = self.TimeSinceLastSpawn + delta
-    Log.Info("Time since last spawn: " .. tostring(self.TimeSinceLastSpawn) .. " seconds. Spawn interval: " .. tostring(self.SpawnInterval) .. " seconds. Active Zombies: " .. tostring(self.ActiveZombies) .. "/" .. tostring(self.MaxConcurrentZombies))
 
     -- Time to spawn! Check concurrency limits first.
     if self.TimeSinceLastSpawn >= self.SpawnInterval then
-        Log.Info("SpawnManager: Time to spawn a new zombie!")
         if self.ActiveZombies < self.MaxConcurrentZombies then
-            Log.Info("SpawnManager: Active zombies below max limit, spawning new zombie.")
             self.TimeSinceLastSpawn = 0.0
             self:TriggerRandomSpawner()
         end
@@ -78,20 +72,15 @@ function SpawnManager:TriggerRandomSpawner()
         return
     end
 
-    Log.Info("SpawnManager: Triggering a random spawner for round " .. tostring(self.RoundNum) .. ". Zombies spawned this wave: " .. tostring(self.ZombiesSpawned) .. "/" .. tostring(self.TotalZombiesForWave))
-
     local randomIndex = Math.RandomInt(1, #self.Spawners)
     local spawnerEntity = self.Spawners[randomIndex]
     if spawnerEntity and spawnerEntity:IsValid() then
         local spawnerScript = spawnerEntity:GetScriptInstance()
         if spawnerScript and spawnerScript.Spawn then
-            Log.Info("SpawnManager triggering spawner: " .. spawnerEntity:GetName())
             spawnerScript:Spawn(spawnerEntity, self.RoundNum)
 
             self.ZombiesSpawned = self.ZombiesSpawned + 1
             self.ActiveZombies = self.ActiveZombies + 1
-            Log.Info("SpawnManager: Zombies Spawned = " .. tostring(self.ZombiesSpawned) .. ", Active Zombies = " .. tostring(self.ActiveZombies))
-            Log.Info("Zombies remaining to spawn this wave: " .. tostring(self.TotalZombiesForWave - self.ZombiesSpawned))
         else
             Log.Error("Child entity '" .. spawnerEntity:GetName() .. "' is missing a Spawner script!")
         end
