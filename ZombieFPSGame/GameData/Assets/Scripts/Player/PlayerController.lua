02a75a4019d0a99c95e512081c303ad7ca1f0b0a
Connor Shore <cjshore12@gmail.com>
2026-07-31 13:50:18 -0600

    added player hit sound effect

--- ZombieFPSGame/GameData/Assets/Scripts/Player/PlayerController.lua
+++ ZombieFPSGame/GameData/Assets/Scripts/Player/PlayerController.lua
@@ -1,6 +1,7 @@
 local PlayerController = {}
 
 PlayerController.Health = 100
+PlayerController.DamageSound = AudioClipRef()
 
 function PlayerController:OnCreate(entity)
 end
@@ -11,6 +12,10 @@ end
 
 function PlayerController:TakeDamage(amount)
     EventManager.Broadcast("OnPlayerDamaged", amount)
+    
+    if self.DamageSound and self.DamageSound:IsValid() then
+        AudioSystem.PlayOneShot(self.DamageSound)
+    end
 
     self.Health = self.Health - amount
     if self.Health < 0 then
